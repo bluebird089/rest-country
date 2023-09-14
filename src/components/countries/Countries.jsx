eccbbbea16afe34e89da8d33ff5ce15ef622946d
bluebird089 <bluebird089@proton.me>
2023-09-14 21:51:15 +0600

    added last commit

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -5,7 +5,7 @@ import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     const [visitedCountries, setVisitedCountries] = useState([]);
-    const [flag, setFalg] = useState([])
+    const [visitedFlags, setVisitedFlags] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -20,26 +20,35 @@ const Countries = () => {
         setVisitedCountries(newVisitedCountry);
     }
 
-    const handleFalg = flag => {
-        console.log('Add Flag');
+    const handleVisitedFlags = flag =>{
+        const newVisitedFlag = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlag);
     }
+
+    
 
     return (
         <>
             <h3>Countries: {countries.length}</h3>
-            <dir>
+            <div>
                 <h3>Visited Country: {visitedCountries.length}</h3>
                 <ul>
                     {
                         visitedCountries.map(visitedCountry => <li key={visitedCountry.cca3}>{visitedCountry.name.common}</li>)
                     }
                 </ul>
-            </dir>
+                <div className="flag-container">
+                    {
+                        visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+                    }
+                </div>
+            </div>
             <div className="countries">
                 {
                     countries.map(country => <Country
                         key={country.cca3}
                         handleVisitedCountry={handleVisitedCountry}
+                        handleVisitedFlags={handleVisitedFlags}
                         country={country}></Country>)
                 }
             </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country, handleVisitedCountry }) => {
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
     const { name, flags, population, area, cca3 } = country;
     const [visited, setVisited] = useState(false)
     const handleVisited = () => {
@@ -15,7 +15,10 @@ const Country = ({ country, handleVisitedCountry }) => {
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
-            <button onClick={() =>handleVisitedCountry(country)}>Mark Visited</button>
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flags</button>
+            <br />
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited && 'I have visited'}
         </div>
